128486551953a2dacce08185ae20fd8974d4ff97
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-07-18 01:43:13 +0200

    Setup Sentry.io

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import "../styles/main.scss";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn:
+    "https://bde5d04385fb49f9ab0e48a5ed58da89@o421991.ingest.sentry.io/5342596",
+});
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
