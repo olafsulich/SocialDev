1275c356db750a6207527cfa7583da3bd6b3c4c1
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-07-30 01:52:52 +0200

    Change text type in typings for testing library

--- shared/typings/testing-library.ts
+++ shared/typings/testing-library.ts
@@ -1,11 +1,10 @@
 import { RenderOptions, RenderResult } from "@testing-library/react";
-import { Matcher } from "@testing-library/dom/matches";
 
 declare module "@testing-library/react" {
   type OverrideRenderResult = {
-    getByLabelText: <T extends HTMLElement>(text: Matcher) => T;
-    getByText: <T extends HTMLElement>(text: Matcher) => T;
-    getByTestId: <T extends HTMLElement>(text: Matcher) => T;
+    getByLabelText: <T extends HTMLElement>(text: string) => T;
+    getByText: <T extends HTMLElement>(text: string) => T;
+    getByTestId: <T extends HTMLElement>(text: string) => T;
   } & RenderResult;
 
   export function render(
