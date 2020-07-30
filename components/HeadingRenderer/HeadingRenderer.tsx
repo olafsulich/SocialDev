46eadb28dc40072a86aa87896203868bd5642c29
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-07-30 03:44:28 +0200

    Fix types in HeadingRenderer

--- components/HeadingRenderer/HeadingRenderer.tsx
+++ components/HeadingRenderer/HeadingRenderer.tsx
@@ -9,7 +9,12 @@ interface Props {
 const HeadingRenderer: React.FC<Props> = ({ level, children }) => {
   const childrenElements = React.Children.toArray(children);
   const headingText = childrenElements.reduce(flatten, "");
-  const headingSlug = headingText.toLowerCase().replace(/\W/g, "-");
+  const headingSlug = () => {
+    return typeof headingText === "string"
+      ? headingText.toLowerCase().replace(/\W/g, "-")
+      : "";
+  };
+
   return React.createElement("h" + level, { id: headingSlug }, children);
 };
 
