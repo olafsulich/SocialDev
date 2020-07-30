28a65b799f7b32ee1b849f136314b55567192ae7
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-07-30 03:20:59 +0200

    Add types and clean up in utility flatten function

--- shared/utils/flatten.ts
+++ shared/utils/flatten.ts
@@ -1,9 +1,14 @@
 import React from "react";
 
-const flatten = (text: string, child: React.Component) => {
-  return typeof child === "string"
-    ? text + child
-    : React.Children.toArray(child.props.children).reduce(flatten, text);
+const flatten = (
+  text: string,
+  child: React.Component
+): string | React.ReactNode => {
+  const flattenChildren = React.Children.toArray(child.props.children).reduce(
+    flatten,
+    text
+  );
+  return typeof child === "string" ? text + child : flattenChildren;
 };
 
 export { flatten };
