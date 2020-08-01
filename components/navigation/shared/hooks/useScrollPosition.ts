16de32eae434c0abe34f2b5013e9c7257bed82db
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-08-01 03:23:37 +0200

    Restore deleted files

---
+++ components/navigation/shared/hooks/useScrollPosition.ts
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+const useScrollPosition = (scrollPosition: number) => {
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = useCallback(
+    () => (window && window.scrollY > scrollPosition ? setScrolled(true) : setScrolled(false)),
+    [scrollPosition],
+  );
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
+
+  return [scrolled, setScrolled];
+};
+
+export default useScrollPosition;
