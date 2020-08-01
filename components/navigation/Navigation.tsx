3a80afad0a94ce9505f7a8953aa02feb2c8349f2
Olaf Sulich <macbook@macbook-pro-macbook.home>
2020-08-01 03:41:02 +0200

    Change path in Navigation component

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import useScrollPosition from '../navigation/shared/hooks/useScrollPosition';
+import useScrollPosition from './shared/hooks/useScrollPosition';
 
 const SCROLL_POSITION = 20;
 
--- components/navigation/shared/hooks/useScrollPosition.ts
+++ components/navigation/shared/hooks/useScrollPosition.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const useScrollPosition = (scrollPosition: number) => {
   const [scrolled, setScrolled] = useState(false);
